74d1d1a4a4c57ea458334f234a5dbf2d7d2108a5
Jared York <jyorkdev@gmail.com>
2018-05-07 15:44:37 -0500

    feature: create todo-list-component

--- todo-app-front-end/src/components/todo-list/todo-list.jsx
+++ todo-app-front-end/src/components/todo-list/todo-list.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Todo } from '../../components';
-import TodoListContext from '../todo-list-section/todo-list-section';
+import { TodoListContext } from '../todo-list-section/todo-list-section';
 
 const TodoList = () => {
     return (
         <TodoListContext.Consumer>
             {
-                (value) => (
-                    value.todoList.map((todo, index) => (
-                        <Todo />
+                value => {
+                   value.todoList.length > 0 && value.todoList !== null ? value.todoList.map((todo, index) => (
+                        <div>hello</div>
                     ))
-                )
+                    :
+                     null
+                }
             }
         </TodoListContext.Consumer>
     )
