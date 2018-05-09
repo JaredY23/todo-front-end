3d8767d34d7f5a6e952416e2712758b89eef98db
Jared York <jyorkdev@gmail.com>
2018-05-08 22:14:31 -0500

    chore: add components to index.js

--- todo-app-front-end/src/components/index.js
+++ todo-app-front-end/src/components/index.js
@@ -1,9 +1,13 @@
+import AddTodoButton from './add-todo-button/add-todo-button';
+import TemportaryInput from './temporary-input/temporary-input';
 import Todo from './todo/todo';
 import TodoHeader from './todo-header/todo-header';
 import TodoList from './todo-list/todo-list';
 import TodoListSection from './todo-list-section/todo-list-section';
 
 export {
+    AddTodoButton,
+    TemportaryInput,
     Todo,
     TodoHeader,
     TodoList,
