b34ae0b78c1559afc4455fd75ee0b0e97de2d48f
Jared York <jyorkdev@gmail.com>
2018-05-13 16:15:36 -0500

    refactor: temporary input to a class component

--- todo-app-front-end/src/components/temporary-input/temporary-input.jsx
+++ todo-app-front-end/src/components/temporary-input/temporary-input.jsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TodoListContext } from '../todo-list-section/todo-list-section';
 
 import './temporary-input.css';
 
-const TemporaryInput = () => {
-    return (
-        <TodoListContext.Consumer>
-            {
-                value => {
-                    return (
-                        <input 
-                            type='text' 
-                            onChange={value.setInputValue} 
-                            value={value.inputValue} 
-                            placeholder='Add your todo here...'
-                        />
-                    )
+class TemporaryInput extends Component {
+    constructor() {
+        super();
+        this.inputRef = React.createRef();
+        this.focusInput = this.focusInput.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+    handleKeyDown(e, addTodo, inputValue) {
+        if (e.keyCode === 13) {
+            addTodo(inputValue);
+        }
+    }
+
+    componentDidMount() {
+        this.focusInput();
+    }
+
+    focusInput() {
+        this.inputRef.current.focus();
+    }
+
+    render() {
+        return (
+            <TodoListContext.Consumer>
+                {
+                    value => {
+                        return (
+                            <input 
+                                ref={this.inputRef}
+                                className='temporary-input'
+                                type='text' 
+                                onChange={(e) => value.setInputValue(e)} 
+                                value={value.inputValue} 
+                                placeholder='Add your todo here...'
+                                onKeyDown={(e) => this.handleKeyDown(e, value.addTodo, value.inputValue)}
+                            />
+                        )
+                    }
                 }
-            }
-        </TodoListContext.Consumer>
-    )
+            </TodoListContext.Consumer>
+        )
+    }
 }
 
 export default TemporaryInput;
