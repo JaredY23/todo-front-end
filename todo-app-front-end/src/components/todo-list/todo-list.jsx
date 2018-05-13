aca4a21be344e43a10b7db8cffe2f597b2f4df13
Jared York <jyorkdev@gmail.com>
2018-05-13 16:14:49 -0500

    feature: render todos in  todo-list

--- todo-app-front-end/src/components/todo-list/todo-list.jsx
+++ todo-app-front-end/src/components/todo-list/todo-list.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import { Todo, TodoHeader } from '../../components';
+import { Todo, TodoHeader, AddTodoButton, TemportaryInput } from '../../components';
 import { TodoListContext } from '../todo-list-section/todo-list-section';
+
+import './todo-list.css';
 
 const TodoList = () => {
     return (
         <div className='todo-list-container'>
             <TodoHeader />
-            <ul className='todo-list'>
                 <TodoListContext.Consumer>
                     {
                         value => {
-                        value.todoList.length > 0 && value.todoList !== null ? value.todoList.map((todo, index) => (
-                                <div>hello</div>
-                            ))
-                            :
-                            null
+                            return (
+                                <React.Fragment>
+                                    { value.isTemporaryTodoActive ? <TemportaryInput /> : null }
+                                    <ul className='todo-list'>
+                                        {
+                                            value.todoList.length > 0 && value.todoList !== null ? value.todoList.map((todo, index) => (
+                                                <Todo uniqueId={todo.id} name={todo.value} key={`${todo}-${index}`} />
+                                            ))
+                                            :
+                                            null
+                                        }
+                                    </ul>
+                                </React.Fragment>
+                            )
                         }
                     }
                 </TodoListContext.Consumer>
-            </ul>
+            <AddTodoButton />
         </div>
     )
 }
